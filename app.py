fc996b03168903eadc7682f04b5cfbade92924f9
Sachin Joshiya <skjoshiya8930@gmail.com>
2026-07-22 12:19:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def predict_diabetes(pregnancies, glucose, insulin, bmi, age):
 
         prediction = model.predict(input_data)
 
-        if prediction[0] == 0:
+        if prediction[0] == 1:
             return "🩺 Prediction: High Risk of Diabetes (Positive)"
         else:
             return "✅ Prediction: Low Risk of Diabetes (Negative)"
